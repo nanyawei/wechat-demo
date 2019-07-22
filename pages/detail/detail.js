930ddc2214d72950c01fd92ad48c0afee999110a
wyn <13521583387@163.com>
2019-07-22 16:45:04 +0800

    添加扫一扫页面

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    info: ''
   },
 
   /**
@@ -19,7 +19,6 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
@@ -62,5 +61,65 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  picture: function () {
+    const that = this;
+    wx.scanCode({
+      success: (res) => {
+        const url = res.result;
+        console.log(url)
+        const codeUrl = decodeURIComponent(url);
+        console.log(codeUrl)
+
+
+        const page = getQueryString(codeUrl, 'id');
+        console.log(page)
+
+
+        that.setData({
+          info: url
+        });
+        if (!url) {
+          return wx.showToast({
+            title: "未识别出二维码中地址，跳转失败！"
+          });
+        }
+        wx.navigateTo({
+          url: page,
+          success: function(res){
+            // success
+            console.log('res', res)
+          },
+          fail: function() {
+            // fail
+            console.log('fail', fail)
+          },
+          complete: function() {
+            // complete
+            console.log('complete', complete)
+          }
+        })
+      },
+      fail: (err) => {
+        console.log(err);
+        wx.showToast({
+          title: "扫描失败，请重新扫描！"
+        });
+      }
+    });
   }
-})
+});
+
+let getQueryString = function (url, name) {
+  console.log("url = " + url)
+  console.log("name = " + name)
+  var reg = new RegExp('(^|&|/?)' + name + '=([^&|/?]*)(&|/?|$)', 'i')
+  var r = url.substr(1).match(reg)
+  if (r != null) {
+    console.log("r = " + r)
+    console.log("r[2] = " + r[2])
+    return r[2]
+  }
+  return null;
+}
